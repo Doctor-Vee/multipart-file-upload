cf1f468efdb59e9f2194f881d929b200751f11ef
Doctor Vee <victor.chinewubeze@gmail.com>
2021-07-24 22:03:52 +0100

    :computer: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Multipart File Upload Service
+
+This is a simple service that helps you upload files to a folder in your application.
+
+Run the project ... It should start on port 7700
+
+You can access the Swagger documentation through this link
+http://localhost:7700/swagger-ui.html
+
+Had to use an old swagger version because version 3.0.0 refused to show me the option to upload from Swagger.
---
+++ src/main/java/com/doctorvee/tutorials/controller/FilesController.java
@@ -0,0 +1,65 @@
+package com.doctorvee.tutorials.controller;
+
+import com.doctorvee.tutorials.message.ResponseMessage;
+import com.doctorvee.tutorials.model.FileInfo;
+import com.doctorvee.tutorials.service.FileStorageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@CrossOrigin("*")
+@Api(tags = {"Files Controller"})
+public class FilesController {
+
+    @Autowired
+    FileStorageService storageService;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Upload File")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") @ApiParam(value = "The file to be uploaded") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.save(file);
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @GetMapping("/files")
+    @ApiOperation(value = "Get a list of files")
+    public ResponseEntity<List<FileInfo>> getListFiles() {
+        List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder
+                    .fromMethodName(FilesController.class, "getFile", path.getFileName().toString()).build().toString();
+            return new FileInfo(filename, url);
+        }).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ApiOperation(value = "Download a file by name", notes = "Provide the file name of the file you wish to download")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
